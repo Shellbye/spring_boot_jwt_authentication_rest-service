3c1c8fd43dade8b00fceb2a02b19caebf9e49e92
shellbye <shellbye.com@gmail.com>
2018-12-05 14:35:04 +0800

    EnableEurekaClient

--- src/main/java/com/shellbye/rest/ServiceController.java
+++ src/main/java/com/shellbye/rest/ServiceController.java
@@ -1,10 +1,12 @@
 package com.shellbye.rest;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@EnableEurekaClient
 public class ServiceController {
     @RequestMapping("/msg")
     public String getImages() {
